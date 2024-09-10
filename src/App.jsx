bfac93d867bcbcec973a60fa2857932ea570e633
MCampos23 <camposvmiguelangel@gmail.com>
2024-09-10 21:59:41 +0200

    Challenge and result modal components creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import Challenge from './components/Challenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <Challenge title='Easy' targetTime={1} />
+        <Challenge title='Not easy' targetTime={5} />
+        <Challenge title='Getting tough' targetTime={10} />
+        <Challenge title='Pros only' targetTime={15} />
+      </div>
     </>
   );
 }
