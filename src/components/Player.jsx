8df3a0cc29ac8b1797cb513e71bb7c27a7624140
MCampos23 <camposvmiguelangel@gmail.com>
2024-09-11 22:42:04 +0200

    ForwardRef, focus and cleaning coments

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,28 +1,26 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react";
 
 export default function Player() {
-  const playerName = useRef();
+  const playerName = useRef(null);
   const [userName, setUserName] = useState(null);
-  // const [inputValue, setInputValue] = useState(undefined);
-
+ 
   function handleUserNameInput() {
     setUserName(playerName.current.value);
-    playerName.current.value = ''
+    playerName.current.value = "";
   }
-  // function handleInputValue(e) {
-  //   setInputValue(e.target.value)
-  // }
+
+  useEffect(() => {
+    playerName.current.focus();
+  }, []);
 
   return (
     <section id="player">
-      <h2>Welcome {userName ?? 'unknown entity'}</h2>
+      <h2>Welcome {userName ?? "unknown entity"}</h2>
       <p>
         <input
           ref={playerName}
-          // value={inputValue}
           type="text"
-          // onChange={handleInputValue}
         />
         <button onClick={handleUserNameInput}>Set Name</button>
       </p>
